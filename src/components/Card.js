6f60446e2c7f50b3ecd9399e2c450437a9292e12
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2021-06-21 00:56:12 +0000

    fix: ui correction 😎

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import { AiOutlineArrowRight } from 'react-icons/ai';
 
 export const Card = ({image, subtitle, title}) => {
     return (
-        <div className="flex w-2/4 h-24 bg-white text-white rounded-md content-center items-center shadow-md hover:shadow-lg ring-1 ring-transparent hover:ring-blue-500 hover:bg-blue-100  justify-between px-2 mb-5">
+        <div className="flex w-96 h-24 bg-white text-white rounded-md content-center items-center shadow-md hover:shadow-lg ring-1 ring-transparent hover:ring-blue-500 hover:bg-blue-100  justify-between px-5 mb-5">
             <img src={image} alt="Student Logo" />
             <div className="flex flex-col px-0">
                 <span className="text-black font-semibold">{title}</span>
