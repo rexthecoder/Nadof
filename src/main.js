4ead81e8a06941cc0cf1223d1ab9d7c2bf5320af
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2021-07-13 00:52:42 +0000

    fix: responsiveness

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import {
 
 import Onboarding from './pages/registration/onboarding';
 import Registration from './pages/registration/registration';
-import RegistrationInfo from './pages/registration/registrationInfo';
 import RegistrationFinish from './pages/registration/registrationFinish';
+import RegistrationInfo from './pages/registration/registrationInfo';
 import * as serviceWorker from './serviceWorker';
 
 const Main = () => {
@@ -16,7 +16,7 @@ const Main = () => {
   return (
     <AnimatePresence exitBeforeEnter initial = {false}>
       <Switch location={location}  key={location.pathname}>
-        <Route path="/onboarding" exact >
+        <Route path="/" exact >
           <Onboarding />
         </Route>
         <Route path="/registration" exact >
